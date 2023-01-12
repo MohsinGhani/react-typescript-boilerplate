d501b852aa2be1daff619a2a53b49b5b61bcedfa
Mohsin Ghani <Mohsinghani.777@gmail.com>
2023-01-12 19:29:44 +0500

    first commit

---
+++ src/components/Don/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Don = () => {
+  return <div className="chota_don">Mujhko Pehachanlo Mai Hun DON</div>;
+};
+
+export default Don;
---
+++ src/components/Vardaan/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Vardaan = () => {
+  return (
+    <div className='vardaan'>Mujhko mat pehchano mai hun. VARDAAN</div>
+  )
+}
+
+export default Vardaan
